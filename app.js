a2a3ad200bb2cf3a60d73bbad5dee73830c0c4af
cwr.vishal.bhardwaj <cwr.vishal.bhardwaj@asurion.com>
2017-09-26 14:09:54 +0530

    added a new route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var devicecheck = require('./routes/devicechk');
 var app = express();
 
 // view engine setup
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/deviceCheck', devicecheck);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
