bb0db5b17c376c9186e5cde2dad8ef8fe026569f
cwr.vishal.bhardwaj <cwr.vishal.bhardwaj@asurion.com>
2017-09-26 15:22:25 +0530

    added db code

--- routes/devicechk.js
+++ routes/devicechk.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('db', {results: result.rows} ); }
+    });
+  });
 });
 
 module.exports = router;
